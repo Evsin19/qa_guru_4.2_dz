4105f3f0d05dee1a14c8c4f9670edc9be627cb37
Evsin Igor <Evsin19@icloud.com>
2024-04-29 00:42:04 +0300

    fixed

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropTest {
@@ -32,7 +33,7 @@ public class DragAndDropTest {
     void dragAndDropExample(){
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").dragAndDrop(to("#column-b"));
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
     }
